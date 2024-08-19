e03825f00d0e5c1a848adb895cf122cea4c77382
mvij0004 <mvij0004@student.monash.edu>
2024-08-19 10:17:58 +1000

    fixes

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      map
-    </div>
-  )
-}
+  return <div>map</div>;
+};
 
-export default page
+export default page;
