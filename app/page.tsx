a0f212545af3c658a3a4e5c48060117687f460e0
mvij0004 <mvij0004@student.monash.edu>
2024-08-26 20:00:07 +1000

    added footer and changed some layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Card from "./components/Card";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 import { Heading, Row, Col, Container } from "nes-ui-react";
 import Image from "next/image";
@@ -7,9 +8,9 @@ import AusFire from "@/public/aus-fire.webp";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#FFFBF2] mx-auto max-w-screen">
+      <Navigation />
       <main className="h-[70vh] w-full bg-[url('/home-landing.webp')] bg-center bg-cover bg-no-repeat">
-        <Navigation />
         <Hero />
       </main>
       <div className="py-8 flex flex-col items-center justify-center">
@@ -30,6 +31,7 @@ export default function Home() {
         <div className="relative min-w-full min-h-80 bg-[#FFAD4D] bg-opacity-60 rounded-lg"></div>
         <div className="relative min-w-full min-h-80 bg-[#FFAD4D] bg-opacity-60 rounded-lg"></div>
       </div>
+      <Footer />
     </div>
   );
 }
