34af3db5ac56b19700b0ba503ce1abb1d3663987
Siramade Apivirasirikul <siravira08@gmail.com>
2024-08-27 23:04:25 +1000

    Edit header in timeline section

--- app/components/HistoryTimeline.tsx
+++ app/components/HistoryTimeline.tsx
@@ -8,7 +8,7 @@ const HistoryTimeline = (props: Props) => {
     <div className="-my-6">
       <div className="relative pl-8 sm:pl-32 py-6 group">
         <div className="font-caveat font-medium text-2xl text-orange-600 mb-1 sm:mb-0">
-          The origin
+          2019-20 Fire Season
         </div>
         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-orange-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
           <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-orange-600 bg-orange-200 rounded-full">
@@ -29,7 +29,7 @@ const HistoryTimeline = (props: Props) => {
 
       <div className="relative pl-8 sm:pl-32 py-6 group">
         <div className="font-caveat font-medium text-2xl text-orange-600 mb-1 sm:mb-0">
-          The milestone
+          Lancefield-Cobaw Croziers Track Burn
         </div>
         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-orange-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
           <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-orange-600 bg-orange-200 rounded-full">
@@ -54,7 +54,7 @@ const HistoryTimeline = (props: Props) => {
 
       <div className="relative pl-8 sm:pl-32 py-6 group">
         <div className="font-caveat font-medium text-2xl text-orange-600 mb-1 sm:mb-0">
-          The acquisitions
+          Widespread Devastation and Loss Across Victoria
         </div>
         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-orange-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
           <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-orange-600 bg-orange-200 rounded-full">
@@ -75,7 +75,7 @@ const HistoryTimeline = (props: Props) => {
 
       <div className="relative pl-8 sm:pl-32 py-6 group">
         <div className="font-caveat font-medium text-2xl text-orange-600 mb-1 sm:mb-0">
-          The IPO
+          Australia's Deadliest and Most Devastating Firestorm
         </div>
         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-orange-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
           <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-orange-600 bg-orange-200 rounded-full">
@@ -93,7 +93,7 @@ const HistoryTimeline = (props: Props) => {
 
       <div className="relative pl-8 sm:pl-32 py-6 group">
         <div className="font-caveat font-medium text-2xl text-orange-600 mb-1 sm:mb-0">
-          The IPO
+          A Season of Over 1,000 Fires and Devastating Losses
         </div>
         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-orange-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
           <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-orange-600 bg-orange-200 rounded-full">
@@ -116,7 +116,7 @@ const HistoryTimeline = (props: Props) => {
 
       <div className="relative pl-8 sm:pl-32 py-6 group">
         <div className="font-caveat font-medium text-2xl text-orange-600 mb-1 sm:mb-0">
-          The IPO
+          Devastating Fires Across Victoria with Heavy Losses
         </div>
         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-orange-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
           <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-orange-600 bg-orange-200 rounded-full">
