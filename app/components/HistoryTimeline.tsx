0a7990fc87bc331660680588dd2dfa99b39ebe1c
Siramade Apivirasirikul <siravira08@gmail.com>
2024-08-27 22:34:33 +1000

    add timeline section in learning page

---
+++ app/components/HistoryTimeline.tsx
@@ -0,0 +1,141 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+
+type Props = {};
+
+const HistoryTimeline = (props: Props) => {
+  return (
+    <div className="-my-6">
+      <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div className="font-caveat font-medium text-2xl text-orange-600 mb-1 sm:mb-0">
+          The origin
+        </div>
+        <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-orange-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+          <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-orange-600 bg-orange-200 rounded-full">
+            2020
+          </time>
+          <div className="text-xl font-bold text-slate-900">
+            In the 2019-20 fire season (November 2019 until February 2020),
+            Victoria endured extreme fire conditions with over 1.5 million
+            hectares burnt, immeasurable impact on unique environments, 420
+            houses lost, and five fatalities. Communities in East Gippsland were
+            isolated for weeks as thousands of kilometres of roads and critical
+            infrastructure were rendered unserviceable. In addition, the
+            bushfires have had a significant impact on wildlife and
+            biodiversity.
+          </div>
+        </div>
+      </div>
+
+      <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div className="font-caveat font-medium text-2xl text-orange-600 mb-1 sm:mb-0">
+          The milestone
+        </div>
+        <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-orange-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+          <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-orange-600 bg-orange-200 rounded-full">
+            2015
+          </time>
+          <div className="text-xl font-bold text-slate-900">
+            The Lancefield-Cobaw Croziers Track planned burn was ignited on
+            Wednesday 30 September 2015 in the Macedon Ranges Shire in spring
+            2015. The fires breached containment lines on Saturday 3 October and
+            was brought under control overnight with approximately 70 additional
+            hectares burnt. Further breaches of containment lines occurred on
+            Tuesday 6 October and control of the bushfire was transferred from
+            the Midlands District to the Gisborne Incident Control Centre (ICC)
+            that afternoon. The bushfire, when finally contained on Tuesday 13
+            October, had burnt over 3,000ha and destroyed several dwellings,
+            numerous sheds and many kilometres of fencing. It had also impacted
+            upon lifestyles, livestock and livelihoods and caused considerable
+            economic and social upheaval in the surrounding communities.
+          </div>
+        </div>
+      </div>
+
+      <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div className="font-caveat font-medium text-2xl text-orange-600 mb-1 sm:mb-0">
+          The acquisitions
+        </div>
+        <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-orange-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+          <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-orange-600 bg-orange-200 rounded-full">
+            2013
+          </time>
+          <div className="text-xl font-bold text-slate-900">
+            Victoria experienced a significant fire season in 2012–2013. Between
+            December and mid-March, more than 190,000 hectares of public and
+            private land burned. A community member and four firefighters lost
+            their lives, with 46 houses destroyed. Major fires included the
+            Aberfeldy-Donnellys Creek, Harrietville, Chepstowe, and Grampians
+            fires. The Aberfeldy-Donnellys Creek fire was active for six weeks,
+            burning 86,000 hectares. Harrietville fire ran for 55 days, burning
+            37,000 hectares.
+          </div>
+        </div>
+      </div>
+
+      <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div className="font-caveat font-medium text-2xl text-orange-600 mb-1 sm:mb-0">
+          The IPO
+        </div>
+        <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-orange-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+          <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-orange-600 bg-orange-200 rounded-full">
+            2009
+          </time>
+          <div className="text-xl font-bold text-slate-900">
+            The Black Saturday bushfires were the worst in Australia's history,
+            killing 173 people. Almost 80 communities and entire towns were left
+            unrecognisable. The fires burned more than 2,000 properties and 61
+            businesses. Over 430,000 hectares burned, including 70 national
+            parks and reserves and more than 3,550 agricultural facilities.
+          </div>
+        </div>
+      </div>
+
+      <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div className="font-caveat font-medium text-2xl text-orange-600 mb-1 sm:mb-0">
+          The IPO
+        </div>
+        <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-orange-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+          <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-orange-600 bg-orange-200 rounded-full">
+            2007
+          </time>
+          <div className="text-xl font-bold text-slate-900">
+            Fire agencies responded to more than 1,000 fires across Victoria
+            from mid-December 2006 to mid-March 2007. The total area burned
+            exceeded 1,200,000 hectares.The two most serious fires occurred in
+            the northeast (the Great Divide North fire) and Gippsland (the Great
+            Divide South fire). The fires were contained in mid-February after
+            burning for 59 days. The Great Divide North and South fires burned
+            1,048,238 hectares, majority on public land. Other significant fires
+            burning at the same time were the Tawonga Gap fire (33,590 hectares)
+            and the Tatong-Watchbox Creek Track fire (31,810 hectares). There
+            was one death, 51 houses destroyed and 1,741 stock lost.
+          </div>
+        </div>
+      </div>
+
+      <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div className="font-caveat font-medium text-2xl text-orange-600 mb-1 sm:mb-0">
+          The IPO
+        </div>
+        <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-orange-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+          <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-orange-600 bg-orange-200 rounded-full">
+            2006
+          </time>
+          <div className="text-xl font-bold text-slate-900">
+            More than 500 fires broke out across the state between New Year's
+            Eve and the end of January 2006. The fires affected Victorian
+            communities in Stawell (Deep Lead), Yea, Moondarra, Grampians,
+            Kinglake, and Anakie areas. There were four fatalities in these
+            fires. Fifty-seven houses were destroyed, and 359 farm buildings
+            lost. Stock losses totalled more than 64,000. The fires burned about
+            160,000 hectares. About 60 percent of the area was public land and
+            40 percent private property.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HistoryTimeline;
--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,9 +1,28 @@
 "use client";
 
 import React from "react";
+import Navigation from "../components/Navigation";
+import Hero from "../components/Hero";
+import HistoryTimeline from "../components/HistoryTimeline";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="bg-[#FFFBF2] mx-auto max-w-screen">
+      <main className="h-[70vh] w-full bg-[url('/home-landing.webp')] bg-center bg-cover bg-no-repeat">
+        <Navigation />
+        <Hero />
+      </main>
+      <div className="py-8 flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-bold text-black mb-4">
+          History of bushfire in past 20 years
+        </h1>
+        <hr className="w-full max-w-lg border-gray-400" />
+        <div className="p-12">
+          <HistoryTimeline />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
