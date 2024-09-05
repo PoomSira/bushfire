bf8c8a48e20519aa8bf2cd51e5ee76a4cbbdae70
mvij0004 <mvij0004@student.monash.edu>
2024-09-05 16:40:23 +1000

    fixes

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import HistoryTimeline from "../components/HistoryTimeline";
@@ -63,7 +64,7 @@ const page = () => {
       </div>
 
       {/* Tableau Visualizations */}
-      <section className="relative py-12 px-4">
+      <div className="relative py-12 px-4">
         <div className="absolute inset-0 bg-[url('/bushfire-effect.webp')] bg-[#FFFBF2] opacity-20 bg-cover bg-center brightness-75"></div>
         <div className="relative max-w-7xl mx-auto">
           <h2 className="text-3xl font-black text-center text-gray-700 mb-4">
@@ -72,111 +73,154 @@ const page = () => {
           <hr className="w-full max-w-lg border-gray-700 mb-8 mx-auto" />
 
           {/* TableauViz */}
-          <div className="mb-16">
-            <h3 className="text-2xl font-bold mb-4 text-center">
+          <div className="mb-16 relative">
+            <h3 className="text-2xl font-bold mb-4 text-center text-gray-700">
               Bushfire History Timeline
             </h3>
             <div className="lg:flex lg:items-start lg:justify-between">
               <div className="lg:w-2/3 mb-8 lg:mb-0">
                 <TableauViz />
               </div>
-              <div className="lg:w-1/3 lg:pl-8">
-                <h4 className="text-xl font-bold mb-4">Let's Get Started!</h4>
-                <p className="mb-4">
-                  Step 1: Go to the Bushfire Year Box and click ▶
-                </p>
-                <p className="mb-4">
-                  Watch the years pass by and see how the fires have changed.
-                </p>
-                <p className="font-semibold">
-                  Answer: Bushfires have been getting worse over the years
-                  because the climate is getting hotter and drier. This makes it
-                  easier for fires to start and spread.
-                </p>
+              <div className="lg:w-1/3 lg:pl-8 relative">
+                <div className="bg-[#FFE6C5] p-6 rounded-xl shadow-lg relative transition-transform duration-300 ease-in-out transform hover:scale-105">
+                  <h4 className="text-xl font-bold mb-4 text-gray-700">
+                    Let's Get Started!
+                  </h4>
+                  <p className="mb-4 text-lg text-gray-700">
+                    Step 1: Go to the Bushfire Year Box and click ▶
+                  </p>
+                  <p className="mb-4 text-lg text-gray-700">
+                    Watch the years pass by and see how the fires have changed.
+                  </p>
+                  <p className="font-semibold text-lg text-gray-700">
+                    Bushfires have been getting worse over the years because the
+                    climate is getting hotter and drier. This makes it easier
+                    for fires to start and spread.
+                  </p>
+                </div>
+                {/* Koala image below the card */}
+                <div className="mt-8 flex justify-center transition-transform duration-300 ease-in-out transform hover:scale-105">
+                  <Image
+                    src="/koala-t.png"
+                    alt="Koala doctor with presentation"
+                    width={150} // Adjust size as needed
+                    height={150} // Adjust size as needed
+                    className="object-cover animate-moveSideways"
+                  />
+                </div>
               </div>
             </div>
           </div>
 
           {/* TableauPictograph */}
-          <div className="mb-16">
-            <h3 className="text-2xl font-bold mb-4 text-center">
+          <div className="mb-16 relative">
+            <h3 className="text-2xl font-bold mb-4 text-center text-gray-700">
               Schools Affected by Bushfires
             </h3>
             <div className="lg:flex lg:items-start lg:justify-between">
               <div className="lg:w-2/3 mb-8 lg:mb-0">
                 <TableauPictograph />
               </div>
-              <div className="lg:w-1/3 lg:pl-8">
-                <h4 className="text-xl font-bold mb-4">
-                  Can you guess how many schools have been affected by
-                  bushfires?
-                </h4>
-                <p className="mb-4">
-                  667 schools are identified to be at bushfires risk areas
-                </p>
-                <p className="mb-4">What do the risks mean?</p>
-                <ul className="list-disc list-inside">
-                  <li>
-                    Extreme RISK: Big, dangerous fires. Be very careful and stay
-                    safe!
-                  </li>
-                  <li>
-                    High Risk: Strong fires possible. Stay alert and follow
-                    safety rules.
-                  </li>
-                  <li>
-                    Medium Risk: Small fires likely. Still, be careful and
-                    follow fire safety.
-                  </li>
-                  <li>
-                    Low Risk: Very safe from fires. Know safety rules just in
-                    case.
-                  </li>
-                </ul>
+              <div className="lg:w-1/3 lg:pl-8 relative">
+                <div className="bg-[#FFE6C5] p-6 rounded-xl shadow-lg relative transition-transform duration-300 ease-in-out transform hover:scale-105">
+                  <h4 className="text-xl font-bold mb-4 text-gray-700">
+                    Can you guess how many schools have been affected by
+                    bushfires?
+                  </h4>
+                  <p className="mb-4 text-lg text-gray-700">
+                    667 schools are identified to be in bushfire risk areas.
+                  </p>
+                  <p className="mb-4 text-lg text-gray-700">
+                    What do the risks mean?
+                  </p>
+                  <ul className="list-disc list-inside text-lg text-gray-700">
+                    <li>
+                      <b>Extreme RISK:</b> Big, dangerous fires. Be very careful
+                      and stay safe!
+                    </li>
+                    <li>
+                      <b>High Risk:</b> Strong fires possible. Stay alert and
+                      follow safety rules.
+                    </li>
+                    <li>
+                      <b>Medium Risk:</b> Small fires likely. Still, be careful
+                      and follow fire safety.
+                    </li>
+                    <li>
+                      <b>Low Risk:</b> Very safe from fires. Know safety rules
+                      just in case.
+                    </li>
+                  </ul>
+                </div>
+                {/* Kangaroo image below the card */}
+                <div className="mt-8 flex justify-center transition-transform duration-300 ease-in-out transform hover:scale-105">
+                  <Image
+                    src="/kangaroo.png"
+                    alt="Kangaroo with presentation"
+                    width={150} // Adjust size as needed
+                    height={150} // Adjust size as needed
+                    className="object-cover animate-moveSideways"
+                  />
+                </div>
               </div>
             </div>
           </div>
 
           {/* TableauCluster */}
-          <div>
-            <h3 className="text-2xl font-bold mb-4 text-center">
+          <div className="relative">
+            <h3 className="text-2xl font-bold mb-4 text-center text-gray-700">
               Bushfire Risk Areas
             </h3>
             <div className="lg:flex lg:items-start lg:justify-between">
               <div className="lg:w-2/3 mb-8 lg:mb-0">
                 <TableauCluster />
               </div>
-              <div className="lg:w-1/3 lg:pl-8">
-                <h4 className="text-xl font-bold mb-4">
-                  Which areas are mostly at risk of bushfires?
-                </h4>
-                <p className="mb-4">Step 1: Click on High Risk Bubble</p>
-                <p className="mb-4">
-                  Step 2: Hover over the largest bubble inside it
-                </p>
-                <p className="mb-4">
-                  Yarra Ranges have 13 schools at high risk
-                </p>
-                <p className="mb-4">Step 3: Click on Medium Risk Bubble</p>
-                <p className="mb-4">
-                  Step 4: Hover over the largest 2 bubbles inside it
-                </p>
-                <p className="mb-4">
-                  Yarra Ranges and Greater Bendigo have the most schools with
-                  medium risk
-                </p>
-                <h4 className="text-xl font-bold mt-8 mb-4">
-                  Is your school here?
-                </h4>
-                <p className="font-semibold">
-                  If so, time to become a BUSHFIRE SAFETY EXPERT!
-                </p>
+              <div className="lg:w-1/3 lg:pl-8 relative">
+                <div className="bg-[#FFE6C5] p-6 rounded-xl shadow-lg relative transition-transform duration-300 ease-in-out transform hover:scale-105">
+                  <h4 className="text-xl font-bold mb-4 text-gray-700">
+                    Which areas are mostly at risk of bushfires?
+                  </h4>
+                  <p className="mb-4 text-lg text-gray-700">
+                    Step 1: Click on High Risk Bubble
+                  </p>
+                  <p className="mb-4 text-lg text-gray-700">
+                    Step 2: Hover over the largest bubble inside it
+                  </p>
+                  <p className="mb-4 text-lg text-gray-700">
+                    Yarra Ranges have 13 schools at high risk
+                  </p>
+                  <p className="mb-4 text-lg text-gray-700">
+                    Step 3: Click on Medium Risk Bubble
+                  </p>
+                  <p className="mb-4 text-lg text-gray-700">
+                    Step 4: Hover over the largest 2 bubbles inside it
+                  </p>
+                  <p className="mb-4 text-lg text-gray-700">
+                    Yarra Ranges and Greater Bendigo have the most schools with
+                    medium risk
+                  </p>
+                  <h4 className="text-xl font-bold mt-8 mb-4 text-gray-700">
+                    Is your school here?
+                  </h4>
+                  <p className="font-semibold text-lg text-gray-700">
+                    If so, time to become a BUSHFIRE SAFETY EXPERT!
+                  </p>
+                </div>
+                {/* Wombat image below the card */}
+                <div className="mt-8 flex justify-center transition-transform duration-300 ease-in-out transform hover:scale-105">
+                  <Image
+                    src="/wombat.png"
+                    alt="Wombat with presentation"
+                    width={150} // Adjust size as needed
+                    height={150} // Adjust size as needed
+                    className="object-cover animate-moveSideways"
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
-
+      </div>
       <Footer />
     </div>
   );
