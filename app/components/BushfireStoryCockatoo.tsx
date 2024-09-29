1e0d28466b9c70397a869f2d6000d107af6aa981
mvij0004 <mvij0004@student.monash.edu>
2024-09-29 22:22:32 +1000

    added Manya voice to recover section

--- app/components/BushfireStoryCockatoo.tsx
+++ app/components/BushfireStoryCockatoo.tsx
@@ -8,98 +8,98 @@ const BushfireStoryCockatoo = () => {
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155807/Image1_gdiri0.jpg",
       text: "Title: The Rainbow After the Storm",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/In_the_aftermath_1.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611697/Image_1_qutauc.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155808/Image2_xlelc5.jpg",
       text: "In a small town called Cockatoo, there was a terrible bushfire that left everything burnt and blackened.",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/Once_a_vibrant_town_2.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611696/Image_2_waj1c6.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155808/Image3_alfwae.jpg",
       text: "Daisy and Bobby were very sad and scared after the fire.",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/Daisy_and_Bobby_couldnt_3.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611694/Image_3_mnjbws.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155808/Image4_qp1vgc.jpg",
       text: "Their teacher, Ms. McLeod, decided to help them express their feelings by drawing.",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/McLeod_noticed_the_sadnes_4.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611698/Image_4_ek1mct.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155808/Image5_u7pyeb.jpg",
       text: "For days, the children drew pictures of the fire, trying to get all their sadness and fear out of their minds.",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/For_several_days_5.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611697/Image_5_pae2um.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155808/Image6_qwsb4u.jpg",
       text: "But one day, Ms. McLeod asked them to try something different. She encouraged the children to draw something bright and happy instead.",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/After_days_of_drawing_6.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611698/Image_6_quhkyn.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155809/Image7_hj3dip.jpg",
       text: "To her surprise, every child drew the same thing—a rainbow.",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/What_McLeod_didn-t_expect_7.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611698/Image_7_yricys.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155808/Image8_ggeybf.jpg",
       text: "This rainbow symbolized hope and happiness, showing that even after something as scary as a bushfire, there is always a way to feel better.",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/For_the_children_8.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611704/Image_8_udyfqf.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155808/Image9_oorzqh.jpg",
       text: "The experience that Daisy, Bobby, and the children of Cockatoo went through is called trauma.",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/Daisy_Bobby_and_their_9.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611699/Image_9_iiac4l.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155809/Image10_ukpw7y.jpg",
       text: "Trauma happens when something really scary or upsetting occurs, like a bushfire. It can make you feel stressed, sad, or even confused.",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/Trauma_can_affect_people_10.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611705/Image_10_tu7ftu.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155809/Image11_gifq0s.jpg",
       text: "But it’s important to know that everyone experiences trauma differently, and there are ways to feel better over time.",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/Not_everyone_reacts_11.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611703/Image_11_svsvrb.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155809/Image12_niqtd5.jpg",
       text: "Activities like drawing, talking, writing about your experience can help you recover.",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/Finding_ways_to_express_12.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611704/Image_12_qdtiv3.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155809/Image13_po8huu.jpg",
       text: "After doing these activities, think about how you feel. It’s okay to have mixed emotions after something as big as a bushfire.",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/After_spent_time_13.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611705/Image_13_aguzpo.wav",
     },
     {
       image:
         "https://res.cloudinary.com/dxtvfgaud/image/upload/q_auto,f_auto/v1727155809/Image14_ks0n8r.jpg",
       text: "And just like Daisy, Bobby, and the children in Cockatoo, you too can find ways to see the rainbow after the storm!",
       audio:
-        "https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/Remember_just_like_Daisy_14.wav",
+        "https://res.cloudinary.com/dxtvfgaud/video/upload/q_auto:good,f_auto/v1727611702/Image_14_jyevdv.wav",
     },
   ];
 
--- app/help/page.tsx
+++ app/help/page.tsx
@@ -2,26 +2,26 @@
 
 import React from "react";
 import Navigation from "../components/Navigation";
-import Hero from "../components/Hero";
-import BushfireAreaMap from "../components/BushfireAreaMap";
-import TableauViz from "../components/TableauViz";
-import DeadpoolBye from "../components/DeadpoolBye";
 import Footer from "../components/Footer";
 
 const page = () => {
   return (
     <div className="bg-[#FFFBF2] mx-auto max-w-screen">
-      <div>
-        <main className="h-[70vh] w-full bg-[url('https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/home-landing.webp')] bg-center bg-cover bg-no-repeat">
-          <Navigation />
-          <Hero />
-        </main>
-        {/* <div className="w-3/4">
-        <BushfireAreaMap />
-        <TableauViz />
-      </div> */}
+      <Navigation />
+      {/* Hero Section */}
+      <div className="relative isolate h-[90vh] px-6 pt-14 lg:px-8 flex-grow bg-[url('https://cdn.jsdelivr.net/gh/PoomSira/bushfire@main/public/recovery.jpeg')] bg-cover bg-center">
+        {/* Blur Overlay */}
+        <div className="absolute inset-0 bg-black bg-opacity-20 backdrop-filter backdrop-blur-sm"></div>
 
-        <DeadpoolBye />
+        {/* Content Overlay */}
+        <div className="relative mx-auto max-w-3xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+              Help you need with Bushfire Brigade
+              <br />
+            </h1>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
