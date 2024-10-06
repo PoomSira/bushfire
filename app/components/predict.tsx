1d0cbb14c573bb7e37ec8fb8cffbc40da4a57bc0
Siramade Apivirasirikul <siravira08@gmail.com>
2024-10-07 00:06:22 +1100

    test deployment ML 1.4

--- app/components/predict.tsx
+++ app/components/predict.tsx
@@ -41,20 +41,23 @@ export default function Predict() {
     setError(null);
 
     try {
-      //   const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch(
+        "https://www.bushfire-brigade.me/api/predict",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      //   const response = await fetch("/api/predict", {
       //     method: "POST",
       //     headers: {
       //       "Content-Type": "application/json",
       //     },
       //     body: JSON.stringify(formData),
       //   });
-      const response = await fetch("/api/predict", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
 
       if (!response.ok) {
         throw new Error("Something went wrong");
