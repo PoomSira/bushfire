38ecfca405047febb17d6b2d999b303fc4d30241
Siramade Apivirasirikul <siravira08@gmail.com>
2024-10-07 00:32:46 +1100

    add app py

--- backend/app.py
+++ backend/app.py
@@ -17,8 +17,8 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Your frontend URL
-    # allow_origins=["https://www.bushfire-brigade.me"],
+    # allow_origins=["http://localhost:3000"],  # Your frontend URL
+    allow_origins=["https://www.bushfire-brigade.me"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
