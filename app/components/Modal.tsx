4800010b2b6e7e9e5db6e45bdfb1c0bfa16caa26
Siramade Apivirasirikul <siravira08@gmail.com>
2024-10-11 01:29:45 +1100

    pop up

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -34,7 +34,7 @@ const Modal: React.FC<ModalProps> = ({
       : "bg-red-500 hover:bg-red-600";
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg text-center max-w-sm mx-auto">
         <p className="text-lg font-semibold mb-4 text-black">{message}</p>
         <button
